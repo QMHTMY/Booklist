4396972d3bd1e247ac1861c949e21a0ec214768b
shieber <QMH_XB_FLTMY@yahoo.com>
2019-07-24 22:55:32 +0800

    modify files

--- booklist.py
+++ booklist.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
-# coding:utf-8
+# -*- coding: utf-8 -*-
 #
-# Author: Shieber
-# Date: 2019.07.23
+#    Author: Shieber
+#    Date: 2019.07.23
 #
-# 下载豆瓣图书分类标签并生成各分类书籍阅读书单
-# 
+#                             APACHE LICENSE
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+#                            Function Description
+#    下载豆瓣图书分类标签并生成各分类书籍阅读书单
+#
+#    Copyright 2019 
+#    All Rights Reserved!
 
 import re,time
 import requests
