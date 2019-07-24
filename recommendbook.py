98da563c986fab1639cde45efdab75ac6d94ddf7
shieber <QMH_XB_FLTMY@yahoo.com>
2019-07-24 15:24:37 +0800

    modify recommendbook.py

--- recommendbook.py
+++ recommendbook.py
@@ -7,190 +7,204 @@
 # 下载豆瓣图书分类标签并生成各分类书籍阅读书单
 # 
 
-import os,re,time
+import re,time
 import requests
-import subprocess
+import random
 import datetime
+from os import chdir,getcwd
 from bs4 import BeautifulSoup as Soup
 from os.path import basename 
+from subprocess import call
 from multiprocessing import Pool
 
 class DoubanBookList():
     '''豆瓣图书分类生产器'''
     def __init__(self,author='Shieber'):
-        self.root_url1 = 'https://book.douban.com'
-        self.root_url2 = 'https://book.douban.com/tag'
-        self.save_fold = '/home/shieber/Files/gitp/Douban/booklist/'
-        self.headers   = {'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; \
-                           rv:68.0) Gecko/20100101 Firefox/68.0'} 
-        self.datetime  = self.getDateTime() #日期
-        self.author    = author             #pdf创建者姓名
-        self.max       = 20                 #获取最大页数
+        self.rooturl1 = 'https://book.douban.com/tag'               #分类图书主页
+        self.rooturl2 = 'https://book.douban.com'                   #构造分页url的根url
+        self.maxpage  = 20                                          #获取该类书最大页数
+        self.heading  = '豆瓣读书推荐书单'                          #pdf书单抬头信息 
+        self.author   = author                                      #pdf书单作者姓名
+        self.datetime = self.getDateTime()                          #pdf书单创建日期
+        self.storedir = '/home/shieber/Files/gitp/Douban/booklist/' #书单存储位置
+        self.orders   = {                                           #系统调用的指令
+                           'mkdocx':'Text2docx -a',
+                           'mkpdfs':'libreoffice --invisible --convert-to pdf *.docx'
+                        }
+        self.headers  = {
+                           'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; \
+                            rv:68.0) Gecko/20100101 Firefox/68.0',
+                           'Connection':'close'
+                        } 
+        self.ip_list  = ['144.217.229.155', '49.51.195.24', '192.99.191.239', \
+                         '49.51.68.122', '49.51.70.42', '101.4.136.34', \
+                         '124.192.27.246', '192.95.3.198', '203.195.168.154']
+        self.http_ips = ['https://'+ip for ip in self.ip_list]
 
     def getDateTime(self):
         '''获取当前日期'''
         time = datetime.datetime.now()
-        year = str(time.year)
-        mnth = str(time.month)
-        day  = str(time.day)
 
         if time.month < 10:
-            mnth = ''.join(['0',mnth])  
-        if time.day < 10:
-            day = ''.join(['0',day])  
+            mnth = ''.join(['0',str(time.month)])  
+        else:
+            mnth = str(time.month)
 
-        date = ''.join([' ',year,'.',mnth,'.',day,'\n'])
+        if time.day < 10:
+            day  = ''.join(['0',str(time.day)])  
+        else:
+            day  = str(time.day)
+
+        date = ''.join([' ',str(time.year),'.',mnth,'.',day])
         return date
 
-    def getMaxPage(self,book_url):
-        '''获取该分类下图书最后一页的页码以构造所有页的url'''
-        page_resp = requests.get(book_url,headers=self.headers)
-        if 200 != page_resp.status_code:
-            return None
+    def getSpecificInfo(self,info):
+        '''去除文字中的空格和换行符'''
+        if info:
+            info = str(info.getText())
+            info = item.replace(' ','').replace('\n','')
+        else:
+            info = '暂无'
+        return info
 
-        page_resp.encoding = 'utf-8'
-        soup = Soup(page_resp.content,'html.parser')
+    def getInfo(self,soup):
         if not soup:
-            return None 
+            return []
 
-        patn = re.compile(r'/tag/.*?start=\d+&type=T')
-        page_nums = soup.find_all('a',href=patn)
-        if page_nums:
-            maxpage = int(page_nums[-2].getText())
-            return maxpage
-        else:
-            return None
+        Info = []
+        booklist = soup.find_all('li',class_="subject-item")
+        for book in booklist:
+            book  = book.find('div',class_='info')
+            title = self.getSpecificInfo(book.find('a'))
+            authr = self.getSpecificInfo(book.find('div',class_='pub'))
+            score = self.getSpecificInfo(book.find('span',class_='rating_nums'))
+            descs = self.getSpecificInfo(book.find('p'))
 
-    def makePageUrl(self,book_url,maxpage):
-        '''生成该分类下每一页的url，不超过20页'''
-        page_urls = []
+            info  = {
+                        'title':title,
+                        'authr':authr,
+                        'score':score,
+                        'descs':descs
+                    }
 
-        if not maxpage: #若只有一页
-            page_urls.append(book_url)
-        else:
-            for page in range(min(self.max,maxpage)):
-                item = '?start=%d&type=T'%(page*20)
-                url  = ''.join([book_url,item])
-                page_urls.append(url)
+            Info.append(info)
 
-        return page_urls
+        return Info 
 
-    def getBookInfo(self,page_urls):
+    def getBookInfo(self,pageurls):
         '''返回所有书籍的详细信息列表'''
+        if not pageurls:
+            return []
+
         bookinfo = []
-        for page_url in page_urls:
-            page_resp = requests.get(page_url,headers=self.headers)
-            if 200 != page_resp.status_code:
-                return None
-            page_resp.encoding = 'utf-8'
-
-            soup = Soup(page_resp.content,'html.parser')
-            if not soup:
-                return None
-
-            book_list = soup.find_all('li',class_="subject-item")
-            for book in book_list:
-                book  = book.find('div',class_='info')
-
-                info  = book.find('a')
-                title = str(info['title'])
-                info  = book.find('div',class_='pub')
-                author= str(info.getText())
-                author= author.replace(' ','').replace('\n','') #去除空格和换行符
-                info  = book.find('span',class_='rating_nums')
-                score = str(info.getText())
-                info  = book.find('p')
-                desc  = str(info.getText())
-                desc  = desc.replace('\n','')                   #去除换行符
-
-                info  = {
-                            'title':title,
-                            'author':author,
-                            'score':score,
-                            'desc':desc
-                        }
-
-                bookinfo.append(info)
-
+        for url in pageurls:
+            soup = self.getFromUrl(url)
+            info = self.getInfo(soup)
+            bookinfo += info
         return bookinfo 
-    
-    def makeBookTxt(self,url_base,txtname,bookinfo):
-        '''依据bookinfo生成该类别书籍书单的pdf文件'''
-        if not bookinfo:
-            return None
 
-        with open(txtname,'w') as txtObj:
-            txtObj.write(''.join([' '*42,'豆瓣读书推荐书单(',url_base,')\n'])) #标题
-            txtObj.write(''.join([' '*29,self.author,self.datetime]))          #时间
-            for i, dic in enumerate(bookinfo):
-                txtObj.write(''.join([str(i+1),'\n']))                         #书籍序号
-                txtObj.write(''.join(['　　书名：',dic['title'] ,'\n']))
-                txtObj.write(''.join(['　　作者：',dic['author'],'\n']))
-                txtObj.write(''.join(['　　评分：',dic['score'] ,'\n']))
-                txtObj.write(''.join(['　　简介：',dic['desc']  ,'\n\n']))
-
-    def makeBookDocx(self):
-        '''将目录下所有txt转换为docx'''
-        cwd = os.getcwd()
-        os.chdir(self.save_fold)
-        subprocess.call('Text2docx -a',shell=True)
-        os.chdir(cwd)
-        time.sleep(1)
-
-    def makeBookPdf(self):
-        '''将目录下所有docx转换为pdf'''
-        cwd = os.getcwd()
-        os.chdir(self.save_fold)
-        subprocess.call('libreoffice --invisible --convert-to pdf *.docx',shell=True)
-        os.chdir(cwd)
-
-    def getBookUrls(self,prt=False):
-        '''获取所有分类的主页url'''
-        root_resp= requests.get(self.root_url2,headers=self.headers)
-        if 200 != root_resp.status_code:
-            return None
-        root_resp.encoding = 'utf-8'
-        soup = Soup(root_resp.content,'html.parser')
-
+    def getInfoPlusPageUrls(self,url):
+        soup = self.getFromUrl(url)
         if not soup:
-            return None
-        patn = re.compile(r'/tag/.*')
-        book_cates = soup.find_all('a',href=patn)
+            return [],[]
 
-        book_urls  = []
-        for book_cate in book_cates:
-            book_url = ''.join([ self.root_url1, book_cate['href'] ])
-            book_urls.append(book_url) 
+        bookinfo = self.getInfo(soup)
 
-        if prt:              #控制打印出url
-            for url in book_urls[1:]:
-                print(url)
+        pageurls = []
+        pagenums = soup.find_all('a',href=re.compile(r'/tag/.*?start=\d+&type=T'))
+        if pagenums:
+            maxpage = int(pagenums[-2].getText())
+            for page in range(1,min(self.maxpage,maxpage)):
+                item = '?start=%d&type=T'%(page*20)
+                newurl  = ''.join([url,item])
+                pageurls.append(newurl)
 
-        return book_urls[1:] #第0个url不是需要的
+        return bookinfo, pageurls
 
-    def getBookText(self,book_url):
+    def fileTransfer(self,success,order):
+        if not success:
+            return False
+
+        cwd = getcwd()
+        chdir(self.storedir)
+        call(order,shell=True)
+        chdir(cwd)
+
+    def save2Txt(self,url,bookinfos):
+        '''依据bookinfo生成该类别书籍书单的pdf文件'''
+        if not bookinfos:
+            return False
+
+        txtname = ''.join([self.storedir,basename(url),'.txt'])
+        with open(txtname,'w') as txtObj:
+            txtObj.write(''.join([' '*42,self.heading,'(',basename(url),')','\n'])) #标题
+            txtObj.write(''.join([' '*29,self.author,self.datetime,'\n']))          #时间
+            for i, infodic in enumerate(bookinfos):
+                txtObj.write(''.join([str(i+1),'\n']))                              #书籍序号
+                txtObj.write(''.join(['　　书名：', infodic['title'], '\n']))
+                txtObj.write(''.join(['　　作者：', infodic['authr'], '\n']))
+                txtObj.write(''.join(['　　评分：', infodic['score'], '\n']))
+                txtObj.write(''.join(['　　简介：', infodic['descs'], '\n']))
+                txtObj.write('\n')                                                  #书间空行
+
+    def saveBookList(self,url):
         '''获取书籍信息并保存'''
-        url_base  = basename(book_url)
-        txtname   = ''.join([self.save_fold,url_base,'.txt'])
-        maxpage   = self.getMaxPage(book_url)
-        page_urls = self.makePageUrl(book_url,maxpage)
-        bookinfo  = self.getBookInfo(page_urls)
-        self.makeBookTxt(url_base,txtname,bookinfo)
+        bookinfo1, pageurls = self.getInfoPlusPageUrls(url)
+        bookinfo2 = self.getBookInfo(pageurls)
+        bookinfos = bookinfo1 + bookinfo2
+        self.save2Txt(url,bookinfos)
 
-    def multiGet(self,book_urls):
+    def multiSaveBookList(self,urls):
         '''多进程循环获取所有书籍信息并保存'''
-        if not book_urls:
-            return None
+        if urls:
+            pool = Pool(5)
+            for url in urls:
+                pool.apply_async(self.saveBookList,(url,))
+            pool.close()
+            pool.join()
 
-        pool = Pool(10)
-        for book_url in book_urls:
-            pool.apply_async(self.getBookText,(book_url,))
-        pool.close()
-        pool.join()
+            return True
+            #for url in urls[:10]:
+            #    self.saveBookList(url)
+        else:
+            return False
+
+    def getBookCateUrls(self):
+        '''获取所有分类的主页url'''
+        soup = getFromUrl(self.rooturl1)
+        if not soup:
+            return []
+
+        bookurls  = []
+        bookcates = soup.find_all('a',href=re.compile(r'/tag/.*'))
+        for cate in bookcates:
+            bookurl = ''.join([self.rooturl2, cate['href']])
+            bookurls.append(bookurl) 
+
+        return bookurls[1:] #第1个url不需要
+
+    def getFromUrl(self,url,proxy=False):
+        '''prox控制是否开启代理'''
+        requests.session().keep_alive = False
+        if not proxy:
+            resp = requests.get(url,headers=self.headers)
+        else:
+            prox = {'https':random.choice(self.http_ips)}
+            resp = requests.get(url,headers=self.headers,proxies=prox)
+
+        if 200 == resp.status_code:
+            resp.encoding = 'utf-8'
+            soup = Soup(resp.content,'html.parser')
+            if soup:
+                return soup
+            else:
+                return []
+        else:
+            return []
 
 if __name__ == "__main__":
-    booklist  = DoubanBookList(author='Shieber') #可修改，比如author='Trump' 
-    book_urls = booklist.getBookUrls()           #2019.07.23有145个图书类别
-    booklist.multiGet(book_urls)
-    booklist.makeBookDocx()
-    booklist.makeBookPdf()
+    booklist = DoubanBookList()              #可修改，比如author='Trump' 
+    cateurls = booklist.getBookCateUrls()    #2019.07.23有145个图书类别
+    success  = booklist.multiSaveBookList(cateurls)
+    booklist.fileTransfer(success,self.orders['mkdocx'])
+    booklist.fileTransfer(success,self.orders['mkpdfs'])
